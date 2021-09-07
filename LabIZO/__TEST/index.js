95f68a9f22409e84b683d5e5301d903857659f79
louiszen <louiszen.yip@mobinology.com>
2021-09-07 15:45:06 +0800

    IZOArc new

--- LabIZO/__TEST/index.js
+++ LabIZO/__TEST/index.js
@@ -4,3 +4,4 @@ export {default as MsgizoTest} from '../Msgizo/_test/Test';
 export {default as StylizoTest} from '../Stylizo/_test/Test';
 export {default as StepizoTest} from '../Stepizo/_test/Test';
 export {default as AnalytizoTest} from '../Analytizo/_test/Test';
+export {default as FlowizoTest} from '../Flowizo/_test/Test';
