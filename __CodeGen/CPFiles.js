32084f31bbb38250db4d924cb24799ba1bcdeabd
louiszen <louiszen.yip@mobinology.com>
2022-01-27 10:14:27 +0800

    CPFiles

--- __CodeGen/CPFiles.js
+++ __CodeGen/CPFiles.js
@@ -5,6 +5,4 @@ const Fs = require("./Fs");
   console.log("[o] Copied files to .custom-template to root. ");
   Fs.copyFolderRecursiveSync("./src/IZOArc/__CodeGen/cpfiles/.vscode", "./");
   console.log("[o] Copied files to .vscode to root. ");
-  Fs.copyFileSync("./src/IZOArc/__CodeGen/cpfiles/package.json", "./");
-  console.log("[o] Copied files to package.json to root. ");
 })();
--- __CodeGen/UpdateCPFiles.js
+++ __CodeGen/UpdateCPFiles.js
@@ -2,9 +2,7 @@ const Fs = require("./Fs");
 
 ( async () => {
   Fs.copyFolderRecursiveSync("./.custom-template", "./src/IZOArc/__CodeGen/cpfiles/");
-  console.log("[U][v] Updated files to .custom-template to cpfiles. ");
+  console.log("[U][v] Updated files to .custom-template to cpfiles.");
   Fs.copyFolderRecursiveSync("./.vscode", "./src/IZOArc/__CodeGen/cpfiles/");
-  console.log("[U][v] Updated files to .vscode to cpfiles. ");
-  Fs.copyFileSync("./package.json", "./src/IZOArc/__CodeGen/cpfiles/");
-  console.log("[U][v] Updated files to package.json to cpfiles. ");
+  console.log("[U][v] Updated files to .vscode to cpfiles.");
 })();
